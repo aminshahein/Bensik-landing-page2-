013bb65d60eda8684038852e76cca363c79ddd07
aminshahein <109917781+aminshahein@users.noreply.github.com>
2022-12-20 12:35:29 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let toggleButton =document.querySelector(".toggle-button");
+let navbarLinks=document.querySelector(".navbar-links");
+
+toggleButton.addEventListener('click', ()=>{
+
+
+    navbarLinks.classList.toggle ('active')
+
+  
+ 
+})
